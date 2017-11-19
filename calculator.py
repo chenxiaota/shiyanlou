bd7e2761545a4e9eb46ef5f787cf5640ddc143f8
czj01 <czj01@vip.qq.com>
2017-11-19 21:27:25 +0800

    only need write data

--- calculator.py
+++ calculator.py
@@ -10,6 +10,7 @@ import os.path
 #judge
 if len(sys.argv) != 7:
 	print("Parameter Error")
+	exit()
 else:
 	if sys.argv[1] == '-c':
 		config_file = sys.argv[2]
@@ -38,14 +39,14 @@ else:
 	print("op: ",op_file)
 
 	with open(config_file) as file:
-		JiShuL = (file.readline().split('='))[1].strip()
-		JiShuH = (file.readline().split('='))[1].strip()
-		YangLao = (file.readline().split('='))[1].strip()
-		YiLiao = (file.readline().split('='))[1].strip()
-		ShiYe = (file.readline().split('='))[1].strip()
-		GongShang = (file.readline().split('='))[1].strip()
-		ShengYu = (file.readline().split('='))[1].strip()
-		GongJiJin = (file.readline().split('='))[1].strip()
+		JiShuL = float((file.readline().split('='))[1].strip())
+		JiShuH = float((file.readline().split('='))[1].strip())
+		YangLao = float((file.readline().split('='))[1].strip())
+		YiLiao = float((file.readline().split('='))[1].strip())
+		ShiYe = float((file.readline().split('='))[1].strip())
+		GongShang = float((file.readline().split('='))[1].strip())
+		ShengYu = float((file.readline().split('='))[1].strip())
+		GongJiJin = float((file.readline().split('='))[1].strip())
 	print("JiShuL: ",JiShuL)
 	print("JiShuH: ",JiShuH)
 	print("YangLao: ",YangLao)
@@ -66,9 +67,63 @@ else:
 			userDict[user.split(',')[0]] = user.split(',')[1].replace('\n','')
 		for key in userDict:
 			print(key,",",userDict[key])
-			
-			
-'''
+
+	class UserData(object):
+		def __init__(self,userdatafile)
+				
+
+	def taxprint(taxincome):
+		if taxincome <= 0:
+			return format(0,".2f")
+		elif taxincome <= 1500:
+			tax = taxincome * 0.03
+			return tax
+		elif taxincome > 1500 and taxincome <= 4500:
+			tax = taxincome * 0.1 - 105
+			return tax
+		elif taxincome >4500 and taxincome <=9000:
+			tax = taxincome * 0.2 - 555
+			return tax
+		elif taxincome > 9000 and taxincome <= 35000:
+			tax = taxincome * 0.25 - 1005
+			return tax
+		elif taxincome > 35000 and taxincome <= 55000:
+			tax = taxincome * 0.3 * 2755
+			return tax
+		elif taxincome > 55000 and taxincome <= 80000:
+			tax = taxincome * 0.35 - 5505
+			return tax
+		elif taxincome > 80000:
+			tax = taxincome * 0.45 - 13500
+			return tax
+	
+	def dumptofile(self,outputfile):
+		with open
+
+	for arg in userList:
+		taxlist = arg.split(',')[1].replace('\n','')
+		try:
+			salary =int(taxlist)
+		except:
+			print("Parameter Error in user")
+		if salary >= JiShuL and salary <= JiShuH:
+			fund = salary * YangLao + salary * YiLiao + salary * ShiYe + salary * GongShang + salary * ShengYu + salary * GongJiJin
+		else:
+			if salary < JiShuL:
+				fund = JiShuL * YangLao + JiShuL * YiLiao + JiShuL * ShiYe + JiShuL * GongShang + JiShuL * ShengYu + JiShuL * GongJiJin
+			if salary > JiShuH:
+				fund = JiShuH * YangLao + JiShuH * YiLiao + JiShuH * ShiYe + JiShuH * GongShang + JiShuH * ShengYu + JiShuH * GongJiJin
+		print("fund: ",fund)
+		taxincome = salary - fund - 3500
+		print("taxincome: ",taxincome)
+		if taxincome <= 0:
+			tax = 0
+			reallysalary = format(salary - fund,".2f")
+		else:
+			tax = taxprint(taxincome)
+			print("tax: ",tax)
+			reallysalary = format(salary - fund - tax,".2f")
+		print(taxlist[0],reallysalary,sep=':')
 
 def taxprint(taxincome):
 	if taxincome <= 0:
@@ -95,21 +150,3 @@ def taxprint(taxincome):
         	tax = taxincome * 0.45 - 13505
         	return tax
 
-	for arg in sys.argv[1:]:
-		taxlist = arg.split(':')
-		try:
-			salary =int(taxlist[1])
-		except:
-			print("Parameter Error")
-		fund = salary * 0.08 + salary * 0.02 + salary * 0.005 + salary * 0.06
-		#print("fund: ",fund)
-		taxincome = salary - fund - 3500
-		#print("taxincome: ",taxincome)
-		if taxincome <= 0:
-			reallysalary = format(salary - fund,".2f")
-		else: 
-			tax = taxprint(taxincome)
-			#print("tax: ",tax)
-			reallysalary = format(salary - fund - tax,".2f")
-		print(taxlist[0],reallysalary,sep=':')
-'''
